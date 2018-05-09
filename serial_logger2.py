34a4358d0fbc7e39099380e6b23d92f3102c5357
Jed <jed@8600k>
2018-05-08 22:01:49 -0400

    '.'.

---
+++ serial_logger2.py
@@ -0,0 +1,16 @@
+import serial
+
+import datetime
+
+try:
+    datafile = datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat().replace(":","_")+".dat2"
+	while True:
+		with serial.Serial(port="COM5", baudrate=115200) as ser:
+			packet = ser.read_until(terminator=b'\x04')
+			with open("data.dat", "wb") as fid:
+                fid.write(datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat().encode("UTF-8"))
+				fid.write(packet)
+except KeyboardInterrupt:
+	pass
+except:
+	raise
